1caff5590f4a81f7cfdf9e557b6abd7b1ebd0de7
BasilMubarak <122254371+BasilMubarak@users.noreply.github.com>
2023-01-09 10:50:02 +0400

    Add files via upload

---
+++ Chapter 4 - Exercise 5.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['bananas', 'apples', 'mangoes']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'mangoes' in favorite_fruits:
+    print("You really like mangoes!")
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
+if 'peaches' in favorite_fruits:
+    print("You really like peaches!")
